fd711ab203b4f04986dd944e13d01dfc6c16e238
MelanieLeather <35497730+MelanieLeather@users.noreply.github.com>
2019-04-18 16:51:25 +0100

    Add files via upload

---
+++ Database/blog-v4.sql
@@ -0,0 +1,376 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 18, 2019 at 05:11 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.0.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blog`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_comment` (IN `email` VARCHAR(50), IN `postid` INT, IN `content` VARCHAR(250))  NO SQL
+BEGIN
+insert into comment (user_id, post_id, content)
+values((select id from user where email=user.Email),(postid),(content));
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `create_post` (IN `userID` INT, IN `post_title` VARCHAR(50) CHARSET latin1, IN `body` VARCHAR(200) CHARSET latin1, IN `category1` VARCHAR(50) CHARSET latin1, IN `pic_name` VARCHAR(50) CHARSET latin1)  NO SQL
+BEGIN
+insert into image (image_name)
+values(pic_name);
+
+begin
+insert into post (user_id, title, body, category_id, image_id)
+values((userID),(post_title),(body), (select id from category where category1=category.categories),(SELECT id from image where pic_name=image_name));
+
+
+end;
+end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_post` (IN `id` VARCHAR(50) CHARSET latin1)  NO SQL
+BEGIN
+DELETE FROM post
+WHERE id=id;
+end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetUserbyName` (IN `userFirst_name` VARCHAR(50) CHARSET latin1)  BEGIN
+SELECT * 
+FROM user
+WHERE userFirst_name = first_name;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `read_all_posts` ()  NO SQL
+begin
+SELECT title from post;
+end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `read_post` (IN `postID` INT)  NO SQL
+BEGIN
+select title, body, image_name 
+from post 
+inner join image on post.image_id=image.id
+where postID=post.id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `UpdatePassword` (IN `userEmail` VARCHAR(50) CHARSET latin1, IN `oldPassword` VARCHAR(10) CHARSET latin1, IN `newPassword` VARCHAR(10) CHARSET latin1)  BEGIN
+UPDATE user SET PASSWORD = newPassword
+WHERE userEmail = email and oldPassword = password;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `view_comment` (IN `postID` INT)  NO SQL
+BEGIN
+SELECT content FROM COMMENT WHERE post_id=postID;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `categories` varchar(100) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `categories`) VALUES
+(1, 'Destination'),
+(2, 'Eco-travel'),
+(3, 'Family-travel'),
+(4, 'INSPIRATION'),
+(5, 'Practical-tips');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `content` varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE `country` (
+  `id` int(11) NOT NULL,
+  `country` varchar(50) COLLATE latin1_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`id`, `country`) VALUES
+(1, 'USA'),
+(2, 'UK');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image`
+--
+
+CREATE TABLE `image` (
+  `id` int(11) NOT NULL,
+  `image_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `image`
+--
+
+INSERT INTO `image` (`id`, `image_name`) VALUES
+(1, 'plastic.jpg'),
+(2, 'kids.jpeg'),
+(4, 'hello');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(50) COLLATE latin1_general_ci NOT NULL,
+  `body` text COLLATE latin1_general_ci NOT NULL,
+  `published` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `image_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`id`, `user_id`, `title`, `body`, `published`, `created_at`, `updated_at`, `category_id`, `image_id`) VALUES
+(1, 1, 'Less plastic', 'this is the body of \"less plastic\" article', '0000-00-00 00:00:00', '2019-04-17 11:00:26', NULL, 2, 1),
+(2, 1, 'how to travel with kids', 'this is the body of \"how to travel with kids\"', NULL, '2019-04-17 11:03:15', NULL, 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_like`
+--
+
+CREATE TABLE `post_like` (
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `like_count` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `surname` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `username` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `email` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `role` enum('Author','Admin') CHARACTER SET latin1 DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `country_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `first_name`, `surname`, `username`, `email`, `role`, `password`, `created_at`, `updated_at`, `country_id`) VALUES
+(1, 'Lisa', 'Simpson', 'littelady', 'lisa@springfield.com', 'Author', '5imps0n', '2019-04-14 15:15:58', '0000-00-00 00:00:00', 1),
+(2, 'Marge', 'Simpson', 'MamaS', 'Marge@springfield.com', 'Admin', 'Marg33', '2019-04-14 15:15:58', '0000-00-00 00:00:00', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `views`
+--
+
+CREATE TABLE `views` (
+  `post_id` int(11) NOT NULL,
+  `view_count` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `post_id` (`post_id`);
+
+--
+-- Indexes for table `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `image_id` (`image_id`);
+
+--
+-- Indexes for table `post_like`
+--
+ALTER TABLE `post_like`
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `country_id` (`country_id`);
+
+--
+-- Indexes for table `views`
+--
+ALTER TABLE `views`
+  ADD KEY `post_id` (`post_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `country`
+--
+ALTER TABLE `country`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `image`
+--
+ALTER TABLE `image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`);
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `post_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  ADD CONSTRAINT `post_ibfk_3` FOREIGN KEY (`image_id`) REFERENCES `image` (`id`);
+
+--
+-- Constraints for table `post_like`
+--
+ALTER TABLE `post_like`
+  ADD CONSTRAINT `post_like_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`),
+  ADD CONSTRAINT `post_like_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`);
+
+--
+-- Constraints for table `views`
+--
+ALTER TABLE `views`
+  ADD CONSTRAINT `views_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
